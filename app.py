32bd4c018986f1a26e9d5ec8b5266e5d18c632bd
Samuel <sam99dave@gmail.com>
2021-09-23 22:54:21 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,119 @@
+import streamlit as st
+import cv2
+import keras_ocr
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+import time
+import subprocess
+os.system('cmd /k "date"')
+
+PAGE_CONFIG = {"page_title": "StColab.io",
+"page_icon": ":smiley:", "layout": "centered"}
+st.set_page_config(**PAGE_CONFIG)
+
+
+def main():
+    st.title("Awesome Streamlit for ML")
+    st.subheader("How to run streamlit from colab")
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox('Menu', menu)
+    if choice == 'Home':
+            st.subheader("Streamlit From Colab")
+
+    img_file_buffer = st.file_uploader(
+        "Upload an image", type=["png", "jpg", "jpeg"])
+    if img_file_buffer is not None:
+            image = np.array(Image.open(img_file_buffer))
+    # st.image(image, caption=f"Test", use_column_width=True,)
+    cv2.imwrite('/content/yolov5/images/test.jpg', image)
+
+    next_step = False
+    if st.button('RUN'):
+        with st.spinner('Loading yolo and detecting license plate....'):
+            cmd = 'cd yolov5 ; python detect.py --weights weights/best-fp16.tflite --source images/test.jpg --img 640 --save-txt'
+            returned = subprocess.call(cmd, shell=True)
+        st.write('DONEDONEDONE')
+        time.sleep(20)
+        next_step = True
+
+    if next_step == True:
+
+            with open('/content/yolov5/runs/detect/exp/labels/test.txt', 'r') as f:
+                    text = f.readlines()
+            bb = []
+            for line in text:
+                    s = line.split()
+                    x1 = int(s[1])
+                    y1 = int(s[2])
+                    x2 = int(s[3])
+                    y2 = int(s[4])
+                    bb.append((x1, y1, x2, y2))
+
+    img = cv2.imread('/content/yolov5/images/test.jpg')
+    cropped_images = []
+    i = 0
+    for box in bb:
+        x1, y1, x2, y2 = box
+        crop = img[y1:y2, x1:x2]
+        scale_per = 3
+        width = int(crop.shape[1] * scale_per)
+        height = int(crop.shape[0] * scale_per)
+        dim = (width, height)
+        scaled = cv2.resize(crop, dim, interpolation=cv2.INTER_CUBIC)
+        des = cv2.fastNlMeansDenoisingColored(scaled, None, 10, 10, 7, 15)
+        cropped_images.append(des)
+        st.image(des, caption=f"{x1}, {y1}, {x2}, {y2} are the bbox", use_column_width='never')
+        cv2.imwrite(f'/content/yolov5/runs/detect/exp/crp{i}.jpg', crop)
+        i+=1
+    
+    
+    
+
+    pipeline = keras_ocr.pipeline.Pipeline()
+    # images = [ keras_ocr.tools.read('/content/yolov5/runs/detect/exp/test.jpg')]
+    images = cropped_images
+    
+     # Plot the predictions
+    if len(images) > 1:
+        prediction_groups = pipeline.recognize(images)             
+        fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
+        for ax, image, predictions in zip(axs, images, prediction_groups):
+            keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
+        fig.savefig('/content/yolov5/result/res.jpg')
+        res = cv2.imread('/content/yolov5/result/res.jpg')
+        st.image(res, caption='images of lp numbers', use_column_width='never')
+
+    else:
+        images = [des]
+        prediction_groups = pipeline.recognize(images)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        keras_ocr.tools.drawAnnotations(image=images[0], predictions=prediction_groups[0], ax=ax)
+        fig.savefig('/content/yolov5/result/res.jpg')
+        res = cv2.imread('/content/yolov5/result/res.jpg')
+        lp_number = [text for text, box in prediction_groups[0]]
+        st.image(res, caption=f'{lp_number}', use_column_width='never')
+    delete = subprocess.call('rm -r /content/yolov5/runs', shell=True)
+
+
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# keras_ocr.tools.drawAnnotations(image=images[0], predictions=predictions[0], ax=ax)
+    # fig.savefig('/content/yolov5/result/res.jpg')
+    # res = cv2.imread('/content/yolov5/result/res.jpg')
+    # st.image(res, caption='Predicted License Plate Number', use_column_width=True)
+# delete = subprocess.call('rm -r /content/yolov5/runs', shell=True)
+
+    
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ detect.py
@@ -0,0 +1,259 @@
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+import yaml
+from numpy import random
+import numpy as np
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
+from utils.plots import colors, plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+
+
+@torch.no_grad()
+def detect(opt):
+    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    save_dir = increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok)  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    weights = weights[0] if isinstance(weights, list) else weights
+    suffix = Path(weights).suffix
+    if suffix == '.pt':
+        backend = 'pytorch'
+        model = attempt_load(weights, map_location=device)  # load FP32 model
+        stride = int(model.stride.max())  # model stride
+        imgsz = check_img_size(imgsz, s=stride)  # check img_size
+        names = model.module.names if hasattr(model, 'module') else model.names  # class names
+        if half:
+            model.half()  # to FP16
+    else:
+        import tensorflow as tf
+        from tensorflow import keras
+
+        stride = None
+        with open('/content/yolov5/models/custom/licence_data.yaml') as f:
+            names = yaml.load(f, Loader=yaml.FullLoader)['names']  # class names (assume COCO)
+
+        if suffix == '.pb':
+            backend = 'graph_def'
+
+            # https://www.tensorflow.org/guide/migrate#a_graphpb_or_graphpbtxt
+            # https://github.com/leimao/Frozen_Graph_TensorFlow
+            def wrap_frozen_graph(graph_def, inputs, outputs):
+                def _imports_graph_def():
+                    tf.compat.v1.import_graph_def(graph_def, name="")
+
+                wrapped_import = tf.compat.v1.wrap_function(_imports_graph_def, [])
+                import_graph = wrapped_import.graph
+                return wrapped_import.prune(
+                    tf.nest.map_structure(import_graph.as_graph_element, inputs),
+                    tf.nest.map_structure(import_graph.as_graph_element, outputs))
+
+            graph = tf.Graph()
+            graph_def = graph.as_graph_def()
+            graph_def.ParseFromString(open(weights, 'rb').read())
+            frozen_func = wrap_frozen_graph(graph_def=graph_def, inputs="x:0", outputs="Identity:0")
+
+        elif suffix == '.tflite':
+            backend = 'tflite'
+            # Load TFLite model and allocate tensors
+            interpreter = tf.lite.Interpreter(model_path=weights)
+            interpreter.allocate_tensors()
+
+            # Get input and output tensors
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
+
+        else:
+            backend = 'saved_model'
+            model = keras.models.load_model(weights)
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=backend == 'pytorch')
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=backend == 'pytorch')
+
+    # Run inference
+    if device.type != 'cpu' and backend == 'pytorch':
+        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.parameters())))  # run once
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half and backend == 'pytorch' else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = time_synchronized()
+        if backend == 'pytorch':
+            pred = model(img, augment=opt.augment)[0]
+        else:
+            if backend == 'saved_model':
+                pred = model(img.permute(0, 2, 3, 1).cpu().numpy(), training=False).numpy()
+            elif backend == 'graph_def':
+                pred = frozen_func(x=tf.constant(img.permute(0, 2, 3, 1).cpu().numpy())).numpy()
+            elif backend == 'tflite':
+                input_data = img.permute(0, 2, 3, 1).cpu().numpy()
+                if opt.tfl_int8:
+                    scale, zero_point = input_details[0]['quantization']
+                    input_data = input_data / scale + zero_point
+                    input_data = input_data.astype(np.uint8)
+                interpreter.set_tensor(input_details[0]['index'], input_data)
+                interpreter.invoke()
+                pred = interpreter.get_tensor(output_details[0]['index'])
+                if opt.tfl_int8:
+                    scale, zero_point = output_details[0]['quantization']
+                    pred = pred.astype(np.float32)
+                    pred = (pred - zero_point) * scale
+            # Denormalize xywh
+            pred[..., 0] *= imgsz[1]  # x
+            pred[..., 1] *= imgsz[0]  # y
+            pred[..., 2] *= imgsz[1]  # w
+            pred[..., 3] *= imgsz[0]  # h
+            pred = torch.tensor(pred)
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms,
+                                   max_det=opt.max_det)
+        t2 = time_synchronized()
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            imc = im0.copy() if opt.save_crop else im0  # for opt.save_crop
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = xyxy
+                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or opt.save_crop or view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if opt.hide_labels else (names[c] if opt.hide_conf else f'{names[c]} {conf:.2f}')
+                        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=opt.line_thickness)
+                        if opt.save_crop:
+                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+            # Print time (inference + NMS)
+            print(f'{s}Done. ({t2 - t1:.3f}s)')
+
+            # Stream results
+            if view_img:
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)  # 1 millisecond
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                else:  # 'video' or 'stream'
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer.write(im0)
+
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        print(f"Results saved to {save_dir}{s}")
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', nargs='+', type=int, default=[320, 320], help='image size')  # height, width
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum number of detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--tfl-int8', action='store_true', help='use int8 quantized TFLite model')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    opt = parser.parse_args()
+    opt.img_size *= 2 if len(opt.img_size) == 1 else 1  # expand
+    print(opt)
+    check_requirements(exclude=('tensorboard', 'pycocotools', 'thop'))
+
+    if opt.update:  # update all models (to fix SourceChangeWarning)
+        for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
+            detect(opt=opt)
+            strip_optimizer(opt.weights)
+    else:
+        detect(opt=opt)
